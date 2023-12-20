c70fea062c1e66683be283d36a1db4a4077d63a2
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-12-20 03:05:17 +0200

    Add title and subtitle to dook details view.

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:booky_app/core/utils/style.dart';
 import 'package:booky_app/features/home/presentation/views/widgets/custom_book_details_view_app_bar.dart';
 import 'package:booky_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +15,32 @@ class BoolDetailsBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
             child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            "The Jungle Book",
+            style: Styles.textStyle30.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                decoration: TextDecoration.none),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              "Rudyard Kipling",
+              style: Styles.textStyle18.copyWith(
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500),
+            ),
           ),
         ],
       ),

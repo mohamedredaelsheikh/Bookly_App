a370c05234a8883aed4502a11c201f8e37fbc388
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-12-23 15:58:39 +0200

    create fetch feature cubit logic

--- lib/features/home/presentation/manger/fetchfeaturecubit/fetchfeaturebook_cubit.dart
+++ lib/features/home/presentation/manger/fetchfeaturecubit/fetchfeaturebook_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:booky_app/features/home/data/models/book_model/book_model.dart';
+import 'package:booky_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'fetchfeaturebook_state.dart';
 
 class FetchfeaturebookCubit extends Cubit<FetchfeaturebookState> {
-  FetchfeaturebookCubit() : super(FetchfeaturebookInitial());
+  FetchfeaturebookCubit(this.homeRepo) : super(FetchfeaturebookInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBooks() async {
+    var result = await homeRepo.fetchFeatureBook();
+
+    result.fold((failure) {
+      emit(FetchfeaturebookFailure(errorMessage: failure.errormessage));
+    }, (books) {
+      emit(FetchfeaturebookSuccess(books: books));
+    });
+  }
 }
